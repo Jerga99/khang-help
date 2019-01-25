260d9106a68722498b51a5395d18352dad7c5c17
unknown <mkavo92@gmail.com>
2019-01-24 21:01:17 -0600

    created route and styling home

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RentalComponent } from "./components/rental/rental.component";
+import { TempComponent } from "./components/temp/temp.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: RentalComponent },
+  { path: "temp", component: TempComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
