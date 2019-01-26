30772048178d5cac1f811e6eca73c9efffd1cce7
unknown <mkavo92@gmail.com>
2019-01-26 11:25:49 -0600

    nothing change

--- client/src/app/components/rental/rental.component.ts
+++ client/src/app/components/rental/rental.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./rental.component.scss"]
 })
 export class RentalComponent implements OnInit {
+  private rentals: any[];
   constructor() {}
 
   ngOnInit() {}
